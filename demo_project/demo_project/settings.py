62fd9fadff402f3c34911a218c5de107d580b57b
Aditya Nandkhile <adityanandkhile9696.com>
2025-12-18 01:03:30 +0530

    changes in settings.py file related to core

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   'core',
+    'demo_project.core',
     'rest_framework',
  
 ]
